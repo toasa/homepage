c392ea7db7586f70cdd00028f4e5cccb11a64675
遠山篤史 <atsushi.tohyama.160.333@gmail.com>
2019-06-07 15:54:53 +0900

    Update footer position

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -5,19 +5,20 @@
     <meta charset="utf-8"/>
 </head>
 <body>
-    <div class="header">
-        @yield('header')
+    <div class="wrapper">
+        <div class="header">
+            @yield('header')
+        </div>
+
+        @yield('h1_title')
+
+        <div class="container">
+            <div class="content">
+                @yield('content')
+            </div>
+        </div>
     </div>
-
-    @yield('h1_title')
-
-    <div class="container">
-        <div class="content">
-            @yield('content')
-        </div>
-        
-        <div class="footer">
-            <p>Atsushi Tohyama / atsushi.tohyama.160.333 at gmail.com</p>
-        </div>
+    <div class="footer">
+        <p>Atsushi Tohyama / atsushi.tohyama.160.333 at gmail.com</p>
     </div>
 </body>
