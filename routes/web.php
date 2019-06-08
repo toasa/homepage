b3b1a8091364d14b007345572f9436e48365d2bc
遠山篤史 <atsushi.tohyama.160.333@gmail.com>
2019-06-09 07:46:06 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,22 @@
 |
 */
 
+// トップページへ
 Route::get('/', function () {
     return view('index');
 });
 
-// Route::get('/blog', function () {
-//     return view('blog.blog');
-// });
-
-Route::get('/blog', 'BlogController@list')->name('list');
-
-// /blog/form に GET でアクセスすると、Postontroller の form メソッドを実行するという意味
+// ブログトップページ（記事一覧を表示）へ
 // name メソッドでエイリアスをつけることができる
+Route::get('/blog', 'BlogController@list')->name('list');
+// ブログ新規投稿、または各ブログ編集画面へ
 Route::get('/blog/form/{id?}', 'BlogController@form')->name('form');
+// ブログ投稿
 Route::post('/blog/post', 'BlogController@post')->name('post');
+// ブログ削除
 Route::post('/blog/delete', 'BlogController@delete')->name('delete');
 
+// 書いたコードの一覧ページへ
 Route::get('/codes', function () {
     return view('codes');
 });
