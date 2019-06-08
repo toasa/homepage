3f5deec34791328293a35c843a2d55661340b082
遠山篤史 <atsushi.tohyama.160.333@gmail.com>
2019-06-08 19:49:48 +0900

    Add wysiwyg-editer

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -2,6 +2,16 @@
 <head>
     <title>@yield('title')</title>
     <link type="text/css" rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script type="text/javascript" src="/js/tinymce/tinymce.min.js"></script>
+    <script type="text/javascript">
+        tinymce.init({
+            selector : 'textarea',
+            plugins  : 'jbimages link autolink preview',
+            toolbar  : 'bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link jbimages | preview',
+            menubar  : false,
+            relative_urls : false
+        });
+    </script>
     <meta charset="utf-8"/>
 </head>
 <body>
