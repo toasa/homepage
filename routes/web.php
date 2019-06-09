d943a3129941d59c52e51b4cccb8cfc7d4ee6e7f
遠山篤史 <atsushi.tohyama.160.333@gmail.com>
2019-06-09 11:24:03 +0900

    Simplify web.php

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,16 @@ Route::get('/', function () {
     return view('index');
 });
 
-// ブログトップページ（記事一覧を表示）へ
-// name メソッドでエイリアスをつけることができる
-Route::get('/blog', 'BlogController@list')->name('list');
-// ブログ新規投稿、または各ブログ編集画面へ
-Route::get('/blog/form/{id?}', 'BlogController@form')->name('form');
-// ブログ投稿
-Route::post('/blog/post', 'BlogController@post')->name('post');
-// ブログ削除
-Route::post('/blog/delete', 'BlogController@delete')->name('delete');
+Route::prefix('/blog')->group(function() {
+    // ブログトップページ（記事一覧を表示）へ
+    Route::get('', 'BlogController@list')->name('list');
+    // ブログ新規投稿、または各ブログ編集画面へ
+    Route::get('form/{id?}', 'BlogController@form')->name('form');
+    // ブログ投稿
+    Route::post('post', 'BlogController@post')->name('post');
+    // ブログ削除
+    Route::post('delete', 'BlogController@delete')->name('delete');
+});
 
 // 書いたコードの一覧ページへ
 Route::get('/codes', function () {
